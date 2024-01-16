635b35447a97cfa5344cc65b0be8b7d7142ee83e
Leo Mancini <mancini@leo.gd>
2024-01-16 18:07:52 -0500

    testing prod build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          <code className={styles.code}>Test Deploy With New User</code>
+          <code className={styles.code}>Test Deploy As Prod App</code>
         </p>
         <div>
           <a
