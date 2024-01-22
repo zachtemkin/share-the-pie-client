8cc30080df953ff0922dcb6af6cba746fb78cd0e
Leo Mancini <mancini@leo.gd>
2024-01-21 21:49:14 -0500

    setting local server to localhost

--- src/app/hooks/useChooseServer.js
+++ src/app/hooks/useChooseServer.js
@@ -4,8 +4,8 @@ function useChooseServer() {
   let server = {};
 
   if (process.env.NODE_ENV === "development") {
-    server.socket = "wss://zach.local:4858";
-    server.api = "https://zach.local:4000";
+    server.socket = "wss://localhost:4858";
+    server.api = "https://localhost:4000";
   } else {
     server.socket = "wss://sharethepie.app:4858";
     server.api = "https://api.sharethepie.app";
