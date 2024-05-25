2c781b7c4f5a90ed650a3f987eed70c12135e785
Zachary Temkin <zachtemkin@gmail.com>
2024-05-24 20:47:55 -0400

    updates style of itemList item

--- src/app/components/item.jsx
+++ src/app/components/item.jsx
@@ -28,8 +28,9 @@ const ItemWrapper = styled.li`
   }
 
   &.isNotCheckedByMe {
-    color: rgba(0, 0, 0, 1);
-    background: rgba(255, 255, 255, 0.05);
+    color: rgb(75, 75, 75, 1);
+    background: rgba(255, 255, 255, 0.075);
+    /* text-decoration: line-through; */
   }
 `;
 
@@ -54,7 +55,7 @@ const CheckBoxUnchecked = () => {
   );
 };
 
-const CheckBoxChecked = () => {
+const CheckBoxChecked = ({ isNotCheckedByMe }) => {
   return (
     <svg
       width='24'
@@ -64,13 +65,13 @@ const CheckBoxChecked = () => {
       xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M0 6C0 2.68629 2.68629 0 6 0H18C21.3137 0 24 2.68629 24 6V18C24 21.3137 21.3137 24 18 24H6C2.68629 24 0 21.3137 0 18V6Z'
-        fill='black'
+        fill={isNotCheckedByMe ? "rgb(75, 75, 75, 1)" : "#000"}
       />
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M19.3224 7.1578C18.7031 6.53855 17.6991 6.53855 17.0799 7.1578L10.0371 14.2006L7.10479 11.2683C6.48554 10.649 5.48152 10.649 4.86227 11.2683C4.24301 11.8876 4.24301 12.8916 4.86227 13.5108L8.90736 17.5559C8.96581 17.6144 9.02768 17.6673 9.09233 17.7147C9.71324 18.1774 10.5959 18.1269 11.1596 17.5631L19.3224 9.40033C19.9416 8.78107 19.9416 7.77706 19.3224 7.1578Z'
-        fill='white'
+        fill={isNotCheckedByMe ? "#000" : "#fff"}
       />
     </svg>
   );
@@ -88,7 +89,13 @@ const Item = ({ item, handleClick }) => {
       className={`${item.isChecked && "isChecked"} ${
         item.isChecked && !item.isCheckedByMe && "isNotCheckedByMe"
       }`}>
-      {item.isChecked ? <CheckBoxChecked /> : <CheckBoxUnchecked />}
+      {item.isChecked ? (
+        <CheckBoxChecked
+          isNotCheckedByMe={item.isChecked && !item.isCheckedByMe}
+        />
+      ) : (
+        <CheckBoxUnchecked />
+      )}
 
       <Description>{item.description}</Description>
       <Price>{priceUSD}</Price>
