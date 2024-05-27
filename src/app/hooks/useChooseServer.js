d10f9ae6c454066efdc734fcb00c9b74460ef842
Leo Mancini <mancini@leo.gd>
2024-05-26 23:55:19 -0400

    switch to client components

--- src/app/hooks/useChooseServer.js
+++ src/app/hooks/useChooseServer.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
-
 function useChooseServer() {
   let server = {};
 
   if (process.env.NODE_ENV === "development") {
-    server.socket = "wss://leo.local:4858";
-    server.api = "https://leo.local:4000";
+    server.socket = "wss://localhost:4858";
+    server.api = "https://localhost:4000";
   } else {
     server.socket = "wss://sharethepie.app:4858";
     server.api = "https://api.sharethepie.app";
