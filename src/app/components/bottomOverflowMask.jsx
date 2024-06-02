1e571ad719877afa981df4f69925a32df5e94080
Leo Mancini <mancini@leo.gd>
2024-06-01 22:46:44 -0400

    adds bottom overflow mask

---
+++ src/app/components/bottomOverflowMask.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const BottomOverflowMask = styled.div`
+  width: 100%;
+  height: 3rem;
+  position: fixed;
+  bottom: ${(props) => (props.$isStandalone ? 3 : 1)}rem;
+  z-index: 999;
+  pointer-events: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+
+    background-color: transparent;
+    top: -4rem;
+    height: 8rem;
+    margin-left: 1rem;
+    width: calc(100vw - 2rem);
+    border-bottom-left-radius: 2rem;
+    border-bottom-right-radius: 2rem;
+    box-shadow: 0 4rem 0 0 #000;
+    pointer-events: none;
+  }
+`;
+
+export default BottomOverflowMask;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "./AppContext";
 import TopOverflowMask from "@/app/components/topOverflowMask";
+import BottomOverflowMask from "@/app/components/bottomOverflowMask";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <body className={inter.className}>
             <TopOverflowMask />
             {children}
+            <BottomOverflowMask />
           </body>
         </html>
       </ThemeProvider>
