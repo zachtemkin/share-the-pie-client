baf7102c797529fbd6eebd757c9fa22200076c47
Leo Mancini <mancini@leo.gd>
2024-07-07 02:25:55 -0400

    remove useGenerateSocketId

--- src/app/hooks/useGenerateSocketId.js
+++
@@ -1,8 +0,0 @@
-const generateSocketId = () => {
-  const date = Date.now().toString().slice(-8);
-  const random = Math.floor(10000000 + Math.random() * 90000000).toString();
-
-  return `${random}-${date}`;
-};
-
-export default generateSocketId;
