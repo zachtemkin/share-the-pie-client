e2d8c49e0ebac7cf97ebe939784f1c5813727aa6
Leo Mancini <mancini@leo.gd>
2024-07-07 22:15:02 -0400

    updating socket to use chooseServer util

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,22 +1,5 @@
 import { io } from "socket.io-client";
-
-function chooseServer() {
-  let server = {};
-
-  if (process.env.NODE_ENV === "development") {
-    server.socket = "wss://localhost:4858";
-    server.api = "https://localhost:4000";
-    // server.socket = "wss://leo.local:4858";
-    // server.api = "https://leo.local:4000";
-  } else {
-    // server.socket = "wss://sharethepie.app:4858";
-    // server.api = "https://api.sharethepie.app";
-    server.socket = "wss://localhost:4858";
-    server.api = "https://localhost:4000";
-  }
-
-  return server;
-}
+import chooseServer from "./utils/chooseServer";
 
 const server = chooseServer();
 const socket = io(server.socket);
