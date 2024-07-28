25cbef4c3805692e13e3bf882b53057c971ea485
Leo Mancini <mancini@leo.gd>
2024-07-27 21:01:34 -0400

    updating redirects

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/capture-receipt",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
